cd1eea4356022c99232b4874f9894d0ad9924d10
Uesli Keta <44416957+FestiveChicken@users.noreply.github.com>
2023-09-02 15:43:03 -0400

    progress save not much done
    added code to listen to button clicks and print id

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-let firstVariable = 0
-let secondVariable = 0
+let firstVariable = null
+let secondVariable = null
 let operator = ""
+const buttons = document.querySelectorAll('button')
+const buttonPressed = e => {console.log(e.target.id)}
+
+//gets button id and assigns it to variable
+for (let button of buttons) {
+    button.addEventListener("click", buttonPressed);
+    if (firstVariable == null) {
+        firstVariable = buttonPressed
+        console.log(firstVariable)
+    }
+  }
 
 function add(firstVariable,secondVariable) {
 	return firstVariable + secondVariable
@@ -18,16 +29,17 @@ function divide(firstVariable, secondVariable) {
 	return firstVariable / secondVariable
 };
 
-function operate(firstVariable, secondVariable, operator)
+function operate(firstVariable, secondVariable, operator) {
     if (operator == "+") {
-        add(firstVariable, secondVariable)
+        return add(firstVariable, secondVariable)
     }
     else if (operator == "-") {
-        subtract(firstVariable, secondVariable)
+        return subtract(firstVariable, secondVariable)
     }
     else if (operator == "*") {
-        multiply(firstVariable, secondVariable)
+        return multiply(firstVariable, secondVariable)
     }
     else if (operator == "/") {
-        divide(firstVariable, secondVariable)
+        return divide(firstVariable, secondVariable)
     }
+}
